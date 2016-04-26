e5fb2484390a3cf9433699ff50bc00411f0147f6
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2016-04-26 13:42:15 +0200

    Move project to maven

---
+++ src/test/java/eu/geoknow/athenarc/triplegeo/TripleGeoTest.java
@@ -0,0 +1,38 @@
+package eu.geoknow.athenarc.triplegeo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class TripleGeoTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TripleGeoTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TripleGeoTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testTripleGeo()
+    {
+        assertTrue( true );
+    }
+}
